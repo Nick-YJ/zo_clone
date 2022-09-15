d6939abd5771f287859177197c9aaabc7fedacd2
Nick-YJ <jeffreynicholasy.swe@gmail.com>
2022-09-15 18:12:10 +0530

    App creation

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width={600} height={100}/>
+        <h2 className='App-message'>Clone of Zomato coming soon...</h2>
       </header>
     </div>
   );
